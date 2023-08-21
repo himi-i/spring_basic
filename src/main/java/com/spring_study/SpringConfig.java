b43e8e0d858fa505d2d9346a692041551b5515f0
himi <pphm1224@naver.com>
2023-08-21 23:21:44 +0900

    섹션6 스프링 DB 접근 기술

--- src/main/java/com/spring_study/SpringConfig.java
+++ src/main/java/com/spring_study/SpringConfig.java
@@ -1,22 +1,37 @@
 package com.spring_study;
 
+
+import com.spring_study.repository.JdbcMemberRepository;
+import com.spring_study.repository.JdbcTemplateMemberRepository;
 import com.spring_study.repository.MemberRepository;
 import com.spring_study.repository.MemoryMemberRepository;
 import com.spring_study.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
 
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
+
         return new MemberService(memberRepository());
     }
 
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public MemberRepository memberRepository()
+    {
+//         return new MemoryMemberRepository();
+//         return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
 
